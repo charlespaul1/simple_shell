2242cefe76b149304c21d67ad46da77897f2b7c1
charlespaul1 <charlespaulwabomba@gmail.com>
2022-09-08 22:35:35 -0700

    fix

--- checker.c
+++ checker.c
@@ -1,9 +1,10 @@
 #include "shell.h"
 /**
- *checker- checks to see whether command entered is a built in function.
- *@cmd: tokenized user input
- *@buf: line derived from our getline function
- *Return: 1 if cmd excuted 0 if cmd is not executed
+ * checker- checks to see whether command entered is a built in function.
+ * @cmd: tokenized user input
+ *
+ * @buf: line derived from our getline function
+ * Return: 1 if cmd excuted 0 if cmd is not executed
  */
 int checker(char **cmd, char *buf)
 {
